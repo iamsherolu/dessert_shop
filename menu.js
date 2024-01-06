21ffaf00854ce9cbbf4be011d04467c4c33874f3
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2024-01-06 23:58:17 +0800

    20240106-2

--- menu.js
+++ menu.js
@@ -40,3 +40,17 @@
        
        sessionStorage.setItem('selectedDesserts',JSON.stringify(selectedDesserts));
     }   
+
+//點選dessert_menu_btn，可以跳到指定位置，且不會被上方menu擋住 
+function scrollToSection(sectionId) {
+    var targetSection = document.getElementById(sectionId);
+    var dessert_menu_layout_height = document.getElementById('dessert_menu_layout').offsetHeight;
+
+    if (targetSection) {
+        // 調整滾動位置，減去 dessert_menu_layout的高度
+        window.scroll({
+        top: targetSection.offsetTop - dessert_menu_layout_height,
+        behavior: 'smooth'
+        });
+    }
+    }
