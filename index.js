c6d7f02a0d3ea5ef69245318e5936642d0538fde
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2024-01-07 11:42:31 +0800

    20240107-5

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ function show_menu_items(){
 function off_menu_overlay(){
     document.getElementById("show_menu_overlay").style.display="none";
 }
-function show_news_items(){
-    doucument.getElementById("news_items").style.display="block";
-}
